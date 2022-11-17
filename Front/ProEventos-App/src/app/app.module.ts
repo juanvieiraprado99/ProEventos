5f97653c5dfdf312ecd4c42499fd5ec3bb9644f3
juanprado <juan.prado@cacaushow.com.br>
2022-11-17 16:51:06 -0300

    Seção 4: Angular

---
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent, PalestrantesComponent, EventosComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
